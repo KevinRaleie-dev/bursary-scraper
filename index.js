07826c8f2982223ba20d9fb49c7e9b12ce245ea7
KevinRaleie-dev <kkevraleie@gmail.com>
2021-09-02 21:21:49 +0200

    added function for fetching data by passing in a month

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+/**
+ * @description This function makes a get request to the given URL 
+ * and returns the response in the form of an object containing the html content
+ * @param {string} url 
+ * @returns {Promise<object>} axios data object
+ */
 const getBursaryData = async (url) => {
 
     console.log('Busy crawling the web..🕷');
@@ -13,6 +19,12 @@ const getBursaryData = async (url) => {
     }
 }
 
+/**
+ * @description This function is used to parse the html given in the parameter
+ * in order to return the list bursaries, title, bursaries that are always open.
+ * @param {string} data  
+ * @returns {object} {}
+ */
 const parseResult = (data) => {
     const $ = cheerio.load(data);
 
@@ -38,18 +50,56 @@ const parseResult = (data) => {
     }
 }
 
+/**
+ * 
+ * @param {string} bursaryMonth 
+ * @returns {string} URL for the specified month
+ */
+const getBursaryDataByMonth = (bursaryMonth) => {
+
+    let error = '';
+    const bm = bursaryMonth.toLowerCase();
+
+    const getMonths = Array.from({length: 12}, (e, i) => {
+        return new Date(null, i + 1, null).toLocaleDateString("en", {month: "long"});
+    });
+
+    const months = [];
+
+    for (let index = 0; index < getMonths.length; index += 1) {
+        const element = getMonths[index].toLowerCase();
+        months.push(element);
+    }
+
+    const found = months.find((month) => month === bm);
+
+    if (!found) {
+        error = 'Could not find ' + bursaryMonth;
+        return error;
+    }
+
+    const url = `https://www.zabursaries.co.za/bursaries-closing-in-${found}-2021/`
+
+    return url;
+}
+
 async function main() {
-    const url = 'https://www.zabursaries.co.za/bursaries-closing-in-september-2021/';
+
+    const url = getBursaryDataByMonth('September');
     const getData = await getBursaryData(url);
     const {alwaysOpen, bursaries, title} = parseResult(getData);
+
+    const firstTen = bursaries.slice(3, 13);
+
     const data = {
         title,
         allBursaries: bursaries,
-        alwaysOpen
+        alwaysOpen,
+        firstTen,
     }
 
+    console.log(data.allBursaries);
 
-    console.log(data);
 }
 
 main();
