bd8e9a35f29edcbfbfb5ffb9c4075c4f34d71742
KevinRaleie-dev <kkevraleie@gmail.com>
2022-01-29 10:59:25 +0200

    removed unneccessary async keyword in server function

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require('dotenv/config');
 const app = require('./src/main');
 
-(async function server() {
+// eslint-disable-next-line require-jsdoc
+function server() {
   const PORT = process.env.PORT || 5000;
   const serverURL = `http://localhost:${PORT}`;
 
   app.listen(PORT, () => {
     console.log(`Server is running on ${serverURL}`);
   });
-})();
+}
+
+server();
